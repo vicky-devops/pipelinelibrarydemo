2a0bf5fc49b904d6d05797ed1fbc3f7722344bee
vicky-devops <73490219+vicky-devops@users.noreply.github.com>
2021-02-06 22:53:06 -0600

    Create Globalvars.groovy

---
+++ src/com/cleverbuilder/Globalvars.groovy
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env groovy
+package com.cleverbuilder
+
+class GlobalVars {
+   static String foo = "bar"
+
+   // refer to this in a pipeline using:
+   //
+   // import com.cleverbuilder.GlobalVars
+   // println GlobalVars.foo
+}
